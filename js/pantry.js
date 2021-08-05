ccd19b2a79f7a80535515da6affbba12f38952ea
kason <kbraley9@gmail.com>
2021-08-04 18:32:34 -0700

    added more abstraction to the functions. Organization

--- js/pantry.js
+++ js/pantry.js
@@ -22,13 +22,20 @@ function makeElement(tagName, parent, textContent, attributes) {
   return element; // returns the created element
 }
 
+///////////////////////// utitlity functions
+function testValidInput(string) {
+  // regular expression to test input name for just letters
+  const regex = /^[a-za-z]+$/;
+  const testReg = regex.exec(string);
+  return testReg;
+}
+
 ////////////// local storage functions //////////////////
 
 function getLocalStorage(name) {
   const storageData = localStorage.getItem(name);
   if (storageData) {
     const parsedArray = JSON.parse(storageData);
-
     return parsedArray;
   }
 }
@@ -38,26 +45,6 @@ function setLocalStorage(name, array) {
   localStorage.setItem(name, stringifiedArray);
 }
 
-////////////////// rendering functions
-//
-function renderTableButton(value, className, fn) {
-  let btn = document.createElement("input");
-  btn.type = "button";
-
-  btn.className = className;
-  btn.value = value;
-  btn.onclick = fn;
-  return btn;
-}
-
-function deleteItemFromTable(event) {
-  const th = event.target.parentNode;
-  const tr = th.parentNode;
-  tr.parentNode.removeChild(tr);
-  // re render the table?
-  // this function rerenders it automatically it seems, so don't need to RE render
-}
-
 function deleteItemFromStorage(array) {
   // deletes an obj from Local Storage based off the name property
   const currentLocalStorage = getLocalStorage("pantry");
@@ -65,32 +52,42 @@ function deleteItemFromStorage(array) {
   const filtered = currentLocalStorage.filter(function (el) {
     return el.name != array[0];
   });
-
   return filtered; // returns the new filtered array
 }
 
-function deleteItem(event) {
-  deleteItemFromTable(event);
+function checkForDuplicateStorageItem(currentLocalStorage, array) {
+  if (currentLocalStorage) {
+    // check for duplicate item names
+    let quantityBefore;
+    const checkDuplicate = currentLocalStorage.some((elem) => {
+      quantityBefore = elem.quantity;
+      return elem.name.toLowerCase() === array[0].toLowerCase();
+    });
 
-  const currentItemToDelete = event.target.parentNode.parentNode;
-  const currentItemTDs = currentItemToDelete.children;
-
-  // convert the HTMLCollection to an array
-  const newArray = [...currentItemTDs];
-  // remove the first item out of the array (this is the button in the table which we don't need right here)
-  newArray.shift();
-
-  const itemValues = [];
-  for (let td of newArray) {
-    itemValues.push(td.innerHTML);
+    if (checkDuplicate) {
+      return quantityBefore;
+    }
+    return false;
   }
-  setLocalStorage("pantry", deleteItemFromStorage(itemValues));
-
-  // delete obj
-  // delete item from local storage
-  // separate functions?
 }
 
+function updateDuplicateStorageItem(quantityBefore, array) {
+  // remove from local storage to update the quantity then add back
+  // since the input was a duplicate, delete from local storage
+  const updatedQuantity = deleteItemFromStorage(array);
+
+  // get the sum of the previous quantity and the new quantity entered
+  array[1] = parseInt(array[1]) + parseInt(quantityBefore);
+
+  // add the new item to local storage with the updated quantity
+  updatedQuantity.push(new PantryItem(...array)); // push a new PantryItem with the quanitities added together
+  setLocalStorage("pantry", updatedQuantity);
+  renderFromStorage(getLocalStorage("pantry"));
+
+  return; // ???
+}
+
+////////////////// rendering functions
 // table stuff
 // values parameter is an array
 function renderTableRow(values) {
@@ -107,12 +104,41 @@ function renderTableRow(values) {
   }
 }
 
+function renderTableButton(value, className, fn) {
+  let btn = document.createElement("input");
+  btn.type = "button";
+  btn.className = className;
+  btn.value = value;
+  btn.onclick = fn;
+  return btn;
+}
+
 function renderFromStorage(storageData) {
-  const rehydratedValues = [];
-  for (const obj of storageData) {
-    rehydratedValues.push(Object.values(obj));
-  }
+  // creates a new array based on `storageData`s objects' values'
+  const rehydratedValues = Array.from(storageData, (x) => Object.values(x));
   renderTableRow(rehydratedValues); // TODO: change to the values from rehydratedObj
+}
+
+function deleteItemFromTable(event) {
+  const th = event.target.parentNode;
+  const tr = th.parentNode;
+  tr.parentNode.removeChild(tr);
+}
+
+function deleteItem(event) {
+  deleteItemFromTable(event);
+
+  const currentItemToDelete = event.target.parentNode.parentNode;
+  const currentItemTDs = currentItemToDelete.children;
+
+  // convert the HTMLCollection to an array
+  const newArray = [...currentItemTDs];
+  // remove the first item out of the array (this is the button in the table which we don't need right here)
+  newArray.shift();
+
+  // creates a new array based on `newArray`s innerHTML values on exch td
+  const itemValues = Array.from(newArray, (x) => x.innerHTML);
+  setLocalStorage("pantry", deleteItemFromStorage(itemValues));
 }
 
 //////////////// Listeners ////////////////////////
@@ -121,67 +147,46 @@ function formCb(event) {
   // need to append to table elem
   const formData = new FormData(event.target);
   const values = [];
-  const currentLocalStorage = getLocalStorage("pantry");
 
   // iterates through the key and value of the form inputs
   for (const pair of formData.entries()) {
     values.push(pair[1].trim().toLowerCase()); // trims any extra spaces before or after the input
   }
 
-  // use a regular expression to test input name for just letters
-  const regEx = /^[a-zA-Z]+$/;
-  let testReg = regEx.exec(values[0]);
-  if (testReg === null) {
-    alert("bad input");
+  // test regex against product name
+  if (!testValidInput(values[0])) {
+    alert("bad input"); // TODO: change to show underneath the Input element
     return;
   }
 
-  // gets what is in the current local storage array of objects (if any), and add an object into that array and then put that array back into local storage
+  const currentLocalStorage = getLocalStorage("pantry");
   if (currentLocalStorage) {
-    // check for duplicate item names
-    let quantityBefore;
-    const checkDuplicate = currentLocalStorage.some((elem) => {
-      quantityBefore = elem.quantity;
-      return elem.name.toLowerCase() === values[0].toLowerCase();
-    });
-    if (checkDuplicate) {
-      // remove from local storage to update the quantity then add back
-
-      // since the input was a duplicate, delete from local storage
-      const updatedQuantity = deleteItemFromStorage(values);
-      //
-      // get the sum of the previous quantity and the new quantity entered
-      values[1] = parseInt(values[1]) + parseInt(quantityBefore);
-
-      // add the new item to local storage with the updated quantity
-      updatedQuantity.push(new PantryItem(...values)); // push a new PantryItem with the quanitities added together
-      setLocalStorage("pantry", updatedQuantity);
-      renderFromStorage(getLocalStorage("pantry"));
-
-      return;
+    const duplicate = checkForDuplicateStorageItem(currentLocalStorage, values);
+    if (duplicate) {
+      updateDuplicateStorageItem(duplicate, values);
+    } else {
+      currentLocalStorage.push(new PantryItem(...values));
+      setLocalStorage("pantry", currentLocalStorage);
     }
-
-    currentLocalStorage.push(new PantryItem(...values));
-    setLocalStorage("pantry", currentLocalStorage);
   } else {
     // no local storage yet so set it up
     this.push(new PantryItem(...values)); // `this` refers to the bound `pantryObjArray` array
 
     setLocalStorage("pantry", this);
   }
-
-  renderFromStorage(getLocalStorage("pantry")); //currently not clearing clearing the current table
+  renderFromStorage(getLocalStorage("pantry"));
 }
 
 ///////////////// Main ///////////////////
 function main() {
   const form = document.getElementById("addFood");
+  // instantly render data from local storage
   const storageData = getLocalStorage("pantry");
   if (storageData) {
     renderFromStorage(storageData);
   }
 
-  const pantryObjArray = []; // being put in local storage
+  const pantryObjArray = []; // ends up in local storage
   form.addEventListener("submit", formCb.bind(pantryObjArray));
 
   // DEVELOPMENT purposes only
